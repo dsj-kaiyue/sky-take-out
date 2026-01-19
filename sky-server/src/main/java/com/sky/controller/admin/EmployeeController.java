a5111bde3f6d84052a82d5343b0965052f306dc5
Kaiyue <kaile2021@126.com>
2026-01-19 19:11:35 +0800

    启用或者禁用员工账户

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -89,11 +89,30 @@ public class EmployeeController {
         return Result.success();
     }
 
+    /**
+     * 员工分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
     @GetMapping("/page")
     @ApiOperation("员工分页查询接口")
     public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO){
         log.info("员工分页查询：{}", employeePageQueryDTO);
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
+    }
+
+    /**
+     * 员工启用或禁用
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("员工启用或禁用接口")
+    public Result startOrStop(@PathVariable Integer status, Long id) {
+        log.info("员工启用或禁用：{},{}", status,id);
+        employeeService.startOrStop(status, id);
+        return Result.success();
     }
 }
