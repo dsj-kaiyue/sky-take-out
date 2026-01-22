dc1363d7ed6b17e42ee5054a9b10e31aadcce3da
Kaiyue <kaile2021@126.com>
2026-01-22 16:51:55 +0800

    删除菜品业务代码

--- sky-server/src/main/java/com/sky/config/OssConfiguration.java
+++ sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -11,10 +11,6 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 @Configuration
 public class OssConfiguration {
-
-
-
-
     @Bean
     @ConditionalOnMissingBean
     public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties){
--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,11 +1,20 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.constant.MessageConstant;
+import com.sky.constant.StatusConstant;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
+import com.sky.exception.DeletionNotAllowedException;
 import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealDishMapper;
+import com.sky.result.PageResult;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +32,8 @@ public class DishServiceImpl implements DishService {
     @Autowired
     private DishFlavorMapper dishFlavorMapper;
 
+    @Autowired
+    private SetmealDishMapper setmealDishMapper;
     @Transactional
     public void saveWithFlavor(DishDTO dishDTO) {
         Dish dish = new Dish();
@@ -43,5 +54,50 @@ public class DishServiceImpl implements DishService {
             dishFlavorMapper.InsertBatch(flavors);
         }
 
+    }
+    /**
+     * 菜品分页查询
+     *
+     * @param dishPageQueryDTO
+     * @return
+     */
+    public PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO) {
+        PageHelper.startPage(dishPageQueryDTO.getPage(),dishPageQueryDTO.getPageSize());
+        Page<DishVO> page=dishMapper.pageQuery(dishPageQueryDTO);
+        return new PageResult(page.getTotal(),page.getResult());
+    }
+
+    /**
+     * 根据id批量删除菜品
+     * @param ids
+     */
+    @Transactional
+    @Override
+    public void deleteByIds(List<Long> ids) {
+        //判断当前菜品是否可以删除————是否在售、
+        for (Long id : ids) {
+            Dish dish=dishMapper.getByID(id);
+            if (dish.getStatus()== StatusConstant.ENABLE){
+                //当前菜品在售，不能删除，抛出业务异常
+                throw new DeletionNotAllowedException(MessageConstant.DISH_ON_SALE);
+            }
+        }
+
+        //判断当前菜品是否可以删除————判断是否关联了套餐
+        List<Long> setmealIds = setmealDishMapper.getSetmealIdsByDishId(ids);
+        if (setmealIds!=null && setmealIds.size()>0){
+            //当前菜品关联了套餐，不能删除，抛出业务异常
+            throw new DeletionNotAllowedException(MessageConstant.DISH_BE_RELATED_BY_SETMEAL);
+        }
+
+        //删除菜品表中的数据
+        for (Long id : ids) {
+            log.info("删除菜品id:{}",id);
+            dishMapper.deleteById(id);
+        }
+        //删除口味表中的数据
+        for (Long id : ids) {
+            dishFlavorMapper.deleteByDishId(id);
+        }
     }
 }
