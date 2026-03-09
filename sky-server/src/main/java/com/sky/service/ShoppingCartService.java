15660d62e0c11154de50419b7a995a9eef2c5b70
Kaiyue <kaile2021@126.com>
2026-03-09 16:48:59 +0800

    购物车功能代码开发

---
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -0,0 +1,31 @@
+package com.sky.service;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+
+import java.util.List;
+
+public interface ShoppingCartService {
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     */
+    void shoppingCartAdd(ShoppingCartDTO shoppingCartDTO);
+
+    /**
+     * 展示购物车
+     * @return
+     */
+    List<ShoppingCart> showShoppingCart();
+
+    /**
+    * 清空购物车
+    */
+    void cleanShoppingCart();
+
+    /**
+     * 减少购物车
+     * @param shoppingCartDTO
+     */
+    void shoppingCartSub(ShoppingCartDTO shoppingCartDTO);
+}
