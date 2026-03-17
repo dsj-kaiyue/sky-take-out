708026aa4e0067173a6bd6647c5c86f70d81e1ec
Kaiyue <kaile2021@126.com>
2026-03-17 18:17:20 +0800

    用户数据统计功能开发

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -3,6 +3,7 @@ package com.sky.controller.admin;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,11 +24,30 @@ public class ReportController {
     @Autowired
     private ReportService reportService;
 
+    /**
+     * 统计指定时间区域内的营业额数据
+     * @param begin
+     * @param end
+     * @return
+     */
     @GetMapping("/turnoverStatistics")
     public Result<TurnoverReportVO> turnoverStatistics(@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin, @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
         log.info("查询营业额统计数据，begin：{}，end：{}", begin, end);
         return Result.success(reportService.getTurnoverStatistics(begin,end));
     }
+
+    /**
+     * 统计指定时间区域内的用户数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    @GetMapping("/userStatistics")
+    public Result<UserReportVO> userStatistics(@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin, @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
+        log.info("查询用户统计数据，begin：{}，end：{}", begin, end);
+        return Result.success(reportService.getUserStatistics(begin, end));
+    }
+
 
 
 
--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -5,6 +5,8 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.Map;
+
 @Mapper
 public interface UserMapper {
 
@@ -24,4 +26,11 @@ public interface UserMapper {
 
     @Select("SELECT * FROM user WHERE id = #{userId}")
     User getById(Long userId);
+
+    /**
+     * 根据条件查询用户数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
