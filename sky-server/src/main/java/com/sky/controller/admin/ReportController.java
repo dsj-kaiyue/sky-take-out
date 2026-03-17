72ec9cee5b6f18775b3cb944085a2ef569256d67
Kaiyue <kaile2021@126.com>
2026-03-17 21:19:38 +0800

    销量排名统计功能开发

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -3,6 +3,7 @@ package com.sky.controller.admin;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
@@ -61,5 +62,17 @@ public class ReportController {
         return Result.success(reportService.getOrderStatistics(begin, end));
     }
 
+
+    /**
+     * 统计指定时间区域内的销售额前10数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    @GetMapping("/top10")
+    public Result<SalesTop10ReportVO> top10(@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin, @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
+        log.info("查询销售额前10统计数据，begin：{}，end：{}", begin, end);
+        return Result.success(reportService.getSalesTop10(begin, end));
+    }
 
 }
